f9797ed00db7f5ba57d7398f06c5e368ee764311
AryanK-47 <aaryankush05@gmail.com>
2026-09-11 00:59:33 +0530

    Schemas created

--- app/features/Incident/models.py
+++ app/features/Incident/models.py
@@ -1,21 +1,63 @@
 import uuid
-from datetime import datetime,timezone
+from datetime import datetime, timezone
+
+from sqlalchemy import DateTime, ForeignKey, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.database import Base
-from sqlalchemy.orm import Mapped,MappedColumn
-from sqlalchemy import String,Text,ForeignKey,PrimaryKeyConstraint,Integer,DateTime
+
 
 class Incident(Base):
-    __tablename__='incidents'
+    __tablename__ = "incidents"
 
-    id:Mapped[uuid.UUID]=MappedColumn(primary_key=True,default=uuid.uuid4)
-    
-    title:Mapped[str]=MappedColumn(String(200),nullable=False)
-    description:Mapped[str]=MappedColumn(Text,nullable=False)
-    service:Mapped[str]=MappedColumn(String(20),nullable=False)
-    severity:Mapped[str]=MappedColumn(String(10),nullable=False)
-    status:Mapped[str]=MappedColumn(String(15),nullable=False,default="OPEN")
-    cause:Mapped[str]=MappedColumn(Text,nullable=True)
-    created_at:Mapped[datetime]=MappedColumn(DateTime(timezone=True),nullable=False,default=lambda:datetime.now(timezone.utc))
-    created_by:Mapped[uuid.UUID]=MappedColumn(ForeignKey("users.id"),nullable=False)
-    resolved_at: Mapped[datetime | None] = MappedColumn(DateTime(timezone=True),nullable=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True,
+        default=uuid.uuid4
+    )
+
+    title: Mapped[str] = mapped_column(
+        String(200),
+        nullable=False
+    )
+
+    description: Mapped[str] = mapped_column(
+        Text,
+        nullable=False
+    )
+
+    service: Mapped[str] = mapped_column(
+        String(20),
+        nullable=False
+    )
+
+    severity: Mapped[str] = mapped_column(
+        String(10),
+        nullable=False
+    )
+
+    status: Mapped[str] = mapped_column(
+        String(15),
+        nullable=False,
+        default="OPEN"
+    )
+
+    cause: Mapped[str] = mapped_column(
+        Text,
+        nullable=True
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    created_by: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False
+    )
+
+    resolved_at: Mapped[datetime | None] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True
+    )
